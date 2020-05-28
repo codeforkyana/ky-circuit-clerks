15caf6a9f922417aac08917ae3a846551cf7201c
Bret Walker <bw@bretw.com>
2020-05-27 22:04:55 -0400

    Trim address and exclude fax

--- get_clerks.py
+++ get_clerks.py
@@ -1,10 +1,16 @@
+import re
+
 import requests
 import pandas as pd
 
 CIRCUIT_CLERKS_URL = "https://kcoj.kycourts.net/ContactList/Search/Results"
 CIRCUIT_CLERKS_BODY = {"SelectedCategory": "CCC", "SelectedCounty": "All"}
 
+ADDRESS_RE = re.compile(r"^.* Circuit Court Clerk")
+
 if __name__ == "__main__":
     res = requests.post(CIRCUIT_CLERKS_URL, data=CIRCUIT_CLERKS_BODY)
-    clerks_list = pd.read_html(res.text)
-    clerks_list[0].to_csv('clerks.csv')
+    clerks_list_df = pd.read_html(res.text)[0]
+    clerks_list_df['Phone Number'] = clerks_list_df['Phone Number'].apply(lambda pn: pn[:14])
+    clerks_list_df['Address'] = clerks_list_df['Address'].apply(lambda a: ADDRESS_RE.sub("", a).strip())
+    clerks_list_df.to_csv('clerks.csv')
