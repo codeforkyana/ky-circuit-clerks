31fee23b974976adb55e2c19d1b6bdf405732c4a
Bret Walker <bw@bretw.com>
2020-05-27 22:44:36 -0400

    remove redundant clerk info

--- get_clerks.py
+++ get_clerks.py
@@ -6,12 +6,12 @@ import pandas as pd
 CIRCUIT_CLERKS_URL = "https://kcoj.kycourts.net/ContactList/Search/Results"
 CIRCUIT_CLERKS_BODY = {"SelectedCategory": "CCC", "SelectedCounty": "All"}
 
-ADDRESS_RE = re.compile(r"Circuit Court Clerk<br/>\n")
+ADDRESS_RE = re.compile(r">.*Circuit Court Clerk<br/>\n")
 NEW_LINE_RE = re.compile(r"<br/>\n?")
 
 if __name__ == "__main__":
     res = requests.post(CIRCUIT_CLERKS_URL, data=CIRCUIT_CLERKS_BODY)
-    clerks_list_df = pd.read_html(NEW_LINE_RE.sub("*NEW*", ADDRESS_RE.sub("", res.text)))[0]
+    clerks_list_df = pd.read_html(NEW_LINE_RE.sub("*NEW*", ADDRESS_RE.sub(">", res.text)))[0]
     clerks_list_df['Phone Number'] = clerks_list_df['Phone Number'].apply(lambda pn: pn[:14])
     clerks_list_df['CityStateZip'] = clerks_list_df['Address'].apply(lambda a: a.split("*NEW*")[2].strip() if len(a.split("*NEW*")) > 2 else "")
     clerks_list_df['Address2'] = clerks_list_df['Address'].apply(lambda a: a.split("*NEW*")[1].split(",")[0].strip())
